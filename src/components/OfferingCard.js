abab068afd4dec8d9ad3033d9812ec4f9d9dafe8
Harshit Garg <ggharshit1@gmail.com>
2023-04-04 12:48:01 +0530

    OfferingSection Added

---
+++ src/components/OfferingCard.js
@@ -0,0 +1,76 @@
+import { ArrowLongRightIcon } from '@heroicons/react/24/outline'
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+function OfferingCard({ width, height = "30vh", step, stepName, imageURL, title, desc, cost = null }) {
+    return (
+        <div
+            className={`flex flex-col sm:w-[100%] lg:w-[${width}] h-[${height}] gap-5 sm:items-center`}
+        >
+            <div
+                className='bg-[#F2F4F6] flex gap-4 h-[100px] w-[100%] items-center justify-center text-[24px]'
+            >
+                <div
+                    className='flex w-[48px] h-[48px] bg-white text-black rounded-[50%] items-center justify-center'
+                >
+                    {step}
+                </div>
+                <p
+                    className='lg:w-[50%] md:w-[65%] md:text-2xl lg:text-base xl:text-2xl 2xl:text-xl'
+                >
+                    {stepName}
+                </p>
+            </div>
+            <div
+                className={`flex flex-col w-[100%] h-[100%] rounded-md border-[#F2F4F6] border-2 p-4 gap-5 items-center`}
+            >
+                <div
+                    className='flex lg:w-[20vw] sm:w-[100%] h-[50%] items-center justify-center'
+                >
+                    <img
+                        src={imageURL}
+                        height="100%"
+                        width="100%"
+                        alt={`${title}img`}
+                        className='lg:h-[100%] sm:h-[200px] rounded-md object-cover'
+                    />
+                </div>
+                <div
+                    className='flex flex-col gap-3 w-[100%] xl:h-[40%] text-left justify-center'
+                >
+                    <h2
+                        className='text-[#002550] font-[600] text-[28px]'
+                    >
+                        {title}
+                    </h2>
+                    <p
+                        className='text-[#1A1A1A] font-[400] text-[18px]'
+                    >
+                        {desc}
+                    </p>
+                    <div
+                        className='flex items-center justify-between w-[100%]'
+                    >
+                        <Button
+                            variant='outlined'
+                            className='w-[150px] flex text-[#01244A] border-[#01244A] items-center justify-center rounded-md gap-2 p-3'
+                        >
+                            <p>Contact Us </p><ArrowLongRightIcon height={20} />
+                        </Button>
+                        {cost && (
+                            <p
+                                className='text-[#01244A] text-right'
+                            >
+                                INR {cost} /Project
+                            </p>
+                        )}
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default OfferingCard
--- src/sections/OfferingSection.js
+++ src/sections/OfferingSection.js
@@ -1,8 +1,70 @@
 import React from 'react'
 
+import OfferingImg1 from '../assests/img/offering1.png'
+import OfferingImg2 from '../assests/img/offering2.jpg'
+import OfferingImg3 from '../assests/img/offering3.jpg'
+import OfferingCard from '../components/OfferingCard';
+
+const Offerings = [
+  {
+    step: '1',
+    stepName: 'Pre-Booking',
+    image: OfferingImg1,
+    title: "Background verification",
+    desc: "Assuring your capital is in safe hands, we provide detailed verification reports of the project and developer. These are customized and detailed...",
+    cost: "3000"
+  },
+  {
+    step: '2',
+    stepName: 'Post-Booking & Pre-Registration',
+    image: OfferingImg2,
+    title: "Virtual site visit",
+    desc: "It becomes imperative to observe your apartment or plot very closely before paying the booking amount. We are here to assist with an AI-enabled UAV...",
+  },
+  {
+    step: '3',
+    stepName: 'Post-Registration',
+    image: OfferingImg3,
+    title: "Title diligence",
+    desc: "Due diligence is necessary to prevent real estate malpractices which have contributed to more than 20% of the civil cases in India. We have a panel of...",
+  }
+];
+
 function OfferingSection() {
   return (
-    <div>OfferingSection</div>
+    <div
+      id='offerings'
+      className=' flex flex-col gap-4 w-[70vw] items-center justify-center'
+    >
+      <h2
+        className='w-[250px] text-[32px] font-[700] border-b-black border-b-2'
+      >
+        Our Offerings
+      </h2>
+      <p
+        color='#1A1A1A'
+      >
+        This whole purchase journey can be divided into three stages. For more details, <span className='text-[#002550] font-bold cursor-pointer underline'>Click Here</span>
+      </p>
+      <div
+        className='lg:w-[120%] xl:w-[100%] flex justify-between sm:flex-col lg:flex-row gap-4'
+      >
+        {Offerings.map((offering, i) => (
+          <OfferingCard
+            key={i}
+            width="80%"
+            height="200px"
+            step={offering.step}
+            stepName={offering.stepName}
+            imageURL={offering.image}
+            title={offering.title}
+            desc={offering.desc}
+            cost={offering.cost}
+          />
+        ))}
+      </div>
+
+    </div>
   )
 }
 
