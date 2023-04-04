c6639dd9c953933630a647d857cd7483cd63bf39
Harshit Garg <ggharshit1@gmail.com>
2023-04-04 13:27:30 +0530

    TeamSection Added

---
+++ src/components/MemberCard.js
@@ -0,0 +1,54 @@
+import { ArrowLongRightIcon } from '@heroicons/react/24/outline'
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+
+function MemberCard({ width, height, imageURL, name, desc }) {
+    return (
+        <div
+            className={`bg-white flex flex-col sm:w-[100%] lg:w-[${width}] h-[${height}] gap-5 sm:items-center drop-shadow-[10px_10px_5px_rgba(0,0,0,0.16)]`}
+        >
+            <div
+                className={`flex flex-col w-[100%] h-[100%] rounded-md border-[#F2F4F6] border-2 p-4 gap-5 items-center `}
+            >
+                <div
+                    className='flex lg:w-[22vw] xl:w-[20vw] sm:w-[100%] h-[50%] items-center justify-center'
+                >
+                    <img
+                        src={imageURL}
+                        height="100%"
+                        width="100%"
+                        alt={`${name}img`}
+                        className='bg-black lg:h-[100%] sm:h-[200px] rounded-md object-cover'
+                    />
+                </div>
+                <div
+                    className='flex flex-col gap-3 w-[100%] xl:h-[40%] text-left'
+                >
+                    <h2
+                        className='text-[#002550] font-[600] text-[28px]'
+                    >
+                        {name}
+                    </h2>
+                    <p
+                        className='text-[#1A1A1A] font-[400] text-[18px]'
+                    >
+                        {desc}
+                    </p>
+                </div>
+                    <div
+                        className='flex items-center justify-end w-[100%]'
+                    >
+                        <Button
+                            variant='outlined'
+                            className='w-[150px] flex text-[#01244A] border-[#01244A] items-center justify-center rounded-md gap-2 p-3'
+                        >
+                            <p>View More </p><ArrowLongRightIcon height={20} />
+                        </Button>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default MemberCard
--- src/sections/TeamSection.js
+++ src/sections/TeamSection.js
@@ -1,8 +1,55 @@
 import React from 'react'
 
+import member1 from '../assests/img/member1.png'
+import member2 from '../assests/img/member2.jpg'
+import member3 from '../assests/img/member3.jpg'
+
+import MemberCard from '../components/MemberCard'
+
+const Members = [
+  {
+    name: "Brajesh Pathak",
+    desc: "A Chartered Accountant & Company Secretary, has over a decade of experience in managing realty focused venture capital funds, and was heading Azure Capital Advisors Private Limited. He has also been...",
+    imageURL: member1
+  },
+  {
+    name: "Deepak Shukla",
+    desc: "Deepak Shukla has completed his B.Tech (hons.) from the Indian Institute of Technology (IIT) Kharagpur and obtained an MBA from the Indian Institute of Management (IIM) Ahmedabad, India. He has an overall experience...",
+    imageURL: member2 
+  },
+  {
+    name: "Alok Kumar Singh",
+    desc: "Entrepreneur with pedigree from IIT-BHU and IIML, and more than 25 years of experience in management responsibilities in top tier companies including Infosys, Infosys BPO and Azure Capital...",
+    imageURL: member3
+  },
+]
+
 function TeamSection() {
   return (
-    <div>TeamSection</div>
+    <div
+      id='team'
+      className=' flex flex-col gap-6 w-[70vw] items-center justify-center'
+    >
+      <h2
+        className=' text-[32px] font-[700] border-b-black border-b-2'
+      >
+        Our Team Members
+      </h2>
+      <div
+          className='lg:w-[120%] xl:w-[100%] xl:h-[78vh] flex justify-between sm:flex-col lg:flex-row gap-4'
+        >
+          {Members.map((member, i) => (
+            <MemberCard
+              key={i}
+              width="80%"
+              height="200px"
+              imageURL={member.imageURL}
+              name={member.name}
+              desc={member.desc}
+            />
+          ))}
+        </div>
+    </div>
   )
 }
 
